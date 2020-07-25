34f7592f4274775f65def0ca73ea860db5784659
techoc <>
2020-07-25 23:33:33 +0800

    macro宏定义

--- app.py
+++ app.py
@@ -112,5 +112,20 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+
+@app.route('/macro')
+def use_macro():
+    return render_template('macro/macro1.html')
+
+
+@app.route('/macro1')
+def use_macro1():
+    return render_template('macro/macro2.html')
+
+
 if __name__ == '__main__':
     app.run()
