e9f77d4c527b6dd5d431d399b24e87b6fda23f38
techoc <543938801@qq.com>
2020-07-30 18:32:30 +0800

    2020-7-30

--- flaskblog/apps/article/models.py
+++ flaskblog/apps/article/models.py
@@ -3,6 +3,13 @@ from datetime import datetime
 from exts import db
 
 
+class Article_type(db.Model):
+    __tablename__ = 'type'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    type_name = db.Column(db.String(20), nullable=False)
+    articles = db.relationship('Article', backref='articletype')
+
+
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(50), nullable=False)
@@ -13,3 +20,16 @@ class Article(db.Model):
     love_num = db.Column(db.Integer, default=0)
     # 外键
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    type_id = db.Column(db.Integer, db.ForeignKey('type.id'), nullable=False)
+    comment = db.relationship('Comment', backref='article')
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    comment = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    article_id = db.Column(db.Integer, db.ForeignKey('article.id'))
+    cdatetime = db.Column(db.DateTime, default=datetime.now)
+
+    def __str__(self):
+        return self.comment
